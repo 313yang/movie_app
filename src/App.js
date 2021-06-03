5aabf1bb22295d82eae14df21de20c337cb05e46
313yang <313yang@naver.com>
2021-06-03 15:04:40 +0900

    use map, key prop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
+function Hobby({ fav, img }) {
+  return (
+    <div>
+      <h2>I love {fav}</h2>
+      <h6>img = {img}</h6>
+    </div>
+  );
 }
-
+const favList = [
+  {
+    id: 1,
+    name: "movie",
+    img: "dfdf",
+  },
+  {
+    id: 2,
+    name: "youtube",
+    img: "uou",
+  },
+  {
+    id: 3,
+    name: "twich",
+    img: "wiwi",
+  },
+];
 function App() {
   return (
     <div>
-      <h1>Hello~~!</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="This is masic" />
-      <Food fav="React power" />
+      {favList.map((fav) => (
+        <Hobby key={fav.id} fav={fav.name} img={fav.img} />
+      ))}
     </div>
   );
 }
