0622e4f3f31ff8034155f1eafd704711bd0fd783
313yang <313yang@naver.com>
2021-06-03 16:54:22 +0900

    class component & setState

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
 import PropTypes from "prop-types";
+import React from "react";
 
-function Hobby({ fav, img }) {
-  return (
-    <div>
-      <h2>I love {fav}</h2>
-      <h6>img = {img}</h6>
-    </div>
-  );
-}
-Hobby.propTypes = {
-  fav: PropTypes.string.isRequired,
-  img: PropTypes.string.isRequired,
-};
-const favList = [
-  {
-    id: 1,
-    name: "movie",
-    img: "dfdf",
-  },
-  {
-    id: 2,
-    name: "youtube",
-    img: "uou",
-  },
-  {
-    id: 3,
-    name: "twich",
-    img: "wiwi",
-  },
-];
-function App() {
-  return (
-    <div>
-      {favList.map((fav) => (
-        <Hobby key={fav.id} fav={fav.name} img={fav.img} />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>This number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
