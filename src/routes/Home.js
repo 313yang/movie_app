6ed4287e8ed17e283b898ddd3009e0e03c351ce2
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-06-14 00:59:51 +0900

    refactoring useing hook

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Movie from "../components/Movie";
 import { Container, Movies } from "../styles/MovieSection";
 import Loading from "../styles/Loading";
 
-class Home extends Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async () => {
+function Home() {
+  const [isLoading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
     const {
       data: {
         data: { movies },
@@ -17,39 +15,33 @@ class Home extends Component {
     } = await axios.get(
       "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=like_count"
     );
-
-    this.setState({ movies, isLoading: false });
+    return setMovies(movies, setLoading(false));
   };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <Container>
-        {isLoading ? (
-          <Loading>
-            <div>Loading...</div>
-          </Loading>
-        ) : (
-          <Movies>
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-                bg={movie.background_image_original}
-              />
-            ))}
-          </Movies>
-        )}
-      </Container>
-    );
-  }
+  useEffect(getMovies);
+  return (
+    <Container>
+      {isLoading ? (
+        <Loading>
+          <div>Loading...</div>
+        </Loading>
+      ) : (
+        <Movies>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+              bg={movie.background_image_original}
+            />
+          ))}
+        </Movies>
+      )}
+    </Container>
+  );
 }
 
 export default Home;
